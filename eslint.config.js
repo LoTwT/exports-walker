e7110516bde38157d33023ff981c6b52fbfa1f4b
LoTwT <709937065@qq.com>
2024-01-02 14:30:29 +0800

    chore: init

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+import { defineFlatConfig } from "@ayingott/eslint-config"
+
+export default defineFlatConfig([], {
+  vue: false,
+  react: false,
+  unocss: false,
+  prettier: true,
+})
